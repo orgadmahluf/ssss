9cdaf7594acad950d90bb7c40c990956bab4f197
orgad <orgad@svcollege.co.il>
2020-07-19 21:24:49 +0300

    sdfd

---
+++ build/precache-manifest.500060761f841e7ee9bdc66ff7da1b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87e37655367357a6f68892bb0c69406",
+    "url": "/ssss/index.html"
+  },
+  {
+    "revision": "1c9aa08a4513dca950b9",
+    "url": "/ssss/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8f09b9edfd47f70ac67",
+    "url": "/ssss/static/js/2.8ecaccd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ssss/static/js/2.8ecaccd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c9aa08a4513dca950b9",
+    "url": "/ssss/static/js/main.b4e0fcb2.chunk.js"
+  },
+  {
+    "revision": "f4c46aa6615953a47982",
+    "url": "/ssss/static/js/runtime-main.87d36d80.js"
+  }
+]);
